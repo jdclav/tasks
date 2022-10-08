4303cbb20b607b005978f85bed4767626a7db5a2
Justin <jdclav@udel.edu>
2022-10-07 20:23:46 -0400

    completed check answers

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 
 export function GiveAttempts(): JSX.Element {
+    const [attempts, setAttempts] = useState<number>(3);
+    const [request, setRequest] = useState<number>(0);
+
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <div>Give Attempts</div>
+            {attempts}
         </div>
     );
 }
